096b09e9fb94cd9b63ac188769036bf2474ac7a9
Victor Mazo <viamazoba@unal.edu.co>
2025-03-05 09:01:08 -0500

    build: Se construye proyecto con vite y react, se instala storybook y se realiza composición con storybook web de chromatic

---
+++ storybook-1/.storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  refs: {
+    'design-system': {
+      title: 'Storybook Chromatic (web)',
+      url: 'https://6660ce016cb2630fa5a9664a-mfssqjpsad.chromatic.com/',
+      expanded: false, // Optional, true by default,
+      //sourceUrl: 'https://github.com/storybookjs/storybook', // Optional
+    },
+  },
+};
+export default config;
